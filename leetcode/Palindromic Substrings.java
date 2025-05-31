0bd24ad2f7f6f2d674de4b56c17a69b366f6207a
shashwat singh <singhsha345@gmail.com>
2025-05-31 18:06:01 +0530

    31 May 2025

---
+++ leetcode/Palindromic Substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0 ;
+
+
+        for( int center = 0 ; center < 2*n-1 ; center++){
+                int left = center / 2;
+                int right = left + center % 2;
+
+                while ( left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                    count++;
+                    left--;
+                    right++;
+                }
+        }
+        return count ;
+    }
+}
